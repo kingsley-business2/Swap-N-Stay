164a88dd5cdd5641a711e3c425e4cc9d4a05af8b
kingsley-business2 <kingsleybusiness2@gmail.com>
2025-10-10 14:10:54 +0000

    Update useAuth.ts

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,7 @@
+// src/hooks/useAuth.ts
+
 import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext'; // Import from the new context file
+import { AuthContext } from '../context/AuthContext';
 
 export const useAuth = () => {
   return useContext(AuthContext);
